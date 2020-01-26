67f90aa98bcb2aa538fb42519a74a152c9f5d7eb
Kyle Talmage <ktalmage85@gmail.com>
2020-01-26 16:27:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,8 @@ sum = first_number + second_number
 difference = second_number - first_number
 product = first_number*second_number
 quotient = second_number/first_number
+
+puts sum
+puts difference
+puts product
+puts quotient
