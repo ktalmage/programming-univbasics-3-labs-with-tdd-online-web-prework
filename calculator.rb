64e746a107538bdcfa50ba6c762717b38d922eaa
Kyle Talmage <ktalmage85@gmail.com>
2020-01-26 16:29:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,7 @@ second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number*second_number
-quotient = second_number/first_number
+quotient = first_number/second_number
 
 puts sum
 puts difference
